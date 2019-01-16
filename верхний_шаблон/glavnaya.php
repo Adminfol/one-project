486b0932fa8f481a712cf0f89ce9d46b6f78aac5
Adminfol <46747547+Adminfol@users.noreply.github.com>
2019-01-16 14:53:28 +0300

    Первая инициализация файла

---
+++ верхний_шаблон/glavnaya.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Главная</title>
+	<link rel="stylesheet" type="text/css" href="glavnaya.css">
+<!--<link rel="stylesheet" type="text/css" href="style.css">-->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
+</head>
+<body>
+<div id="block">
+<div id="block_glavnaya"><div class="text">Главная</div></div>
+<div id="block_napravleniya"><div class="text">Направления</div></div>
+<div id="block_novosty"><div class="text">Новости</div></div>
+<div id="block_kontakty"><div class="text">Контакты</div></div>
+<div id="block_sobytiya"><div class="text">События</div></div>
+<div id="block_lichnyy_kabinet"><div class="text">Личный кабинет</div></div>
+<div id="block_vhod"><div class="text">Вход</div></div>
+<div id="block_registraciya"><div class="text">Регистрация</div></div>	
+</div>
+<script src="glavnaya.js"></script>
+<!--<script src="script.js"></script>-->
+<script src="https://code.jquery.com/jquery-3.1.1.js"></script>
+</body>
+</html>
