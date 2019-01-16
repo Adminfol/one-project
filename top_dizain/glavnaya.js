8b953ca0ab978884f0151f7bb79732390f031bd2
Adminfol <46747547+Adminfol@users.noreply.github.com>
2019-01-16 17:59:20 +0300

    byb

---
+++ top_dizain/glavnaya.js
@@ -0,0 +1,21 @@
+function adaptaciya(){
+$("#block").css({
+"height":$("#block_glavnaya, #block_napravleniya, #block_glavnaya, #block_novosty, #block_kontakty, #block_glavnaya, #block_sobytiya, #block_lichnyy_kabinet, #block_vhod, #block_registraciya").height()+3
+});
+$("#block_glavnaya, #block_napravleniya, #block_glavnaya, #block_novosty, #block_kontakty, #block_glavnaya, #block_sobytiya, #block_lichnyy_kabinet, #block_vhod, #block_registraciya").css({
+"width":$(window).width()/2/2/2-2,
+"height":"40px"
+});
+if($(window).width() >= $(window).height()){
+$(".text").css({
+"font-size":$(window).width()/2/2/2/2/2/2+1.9,
+"margin-top":$("#block_glavnaya, #block_napravleniya, #block_glavnaya, #block_novosty, #block_kontakty, #block_glavnaya, #block_sobytiya, #block_lichnyy_kabinet, #block_vhod, #block_registraciya").height()/5
+});
+}else{
+$(".text").css({
+"font-size":$(window).width()/2/2/2/2/2/2+2.4,
+"margin-top":$("#block_glavnaya, #block_napravleniya, #block_glavnaya, #block_novosty, #block_kontakty, #block_glavnaya, #block_sobytiya, #block_lichnyy_kabinet, #block_vhod, #block_registraciya").height()/5
+});
+}
+}
+setInterval(adaptaciya, 500);
---
+++ top_dizain/glavnaya.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Главная</title>
+	<link rel="stylesheet" type="text/css" href="glavnaya.css">
+<!--<link rel="stylesheet" type="text/css" href="style.css">-->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
+</head>
+<body>
+<div id="block">
+<div id="block_glavnaya"><div class="text">Главная</div></div>
+<div id="block_napravleniya"><div class="text">Направления</div></div>
+<div id="block_novosty"><div class="text">Новости</div></div>
+<div id="block_kontakty"><div class="text">Контакты</div></div>
+<div id="block_sobytiya"><div class="text">События</div></div>
+<div id="block_lichnyy_kabinet"><div class="text">Личный кабинет</div></div>
+<div id="block_vhod"><div class="text">Вход</div></div>
+<div id="block_registraciya"><div class="text">Регистрация</div></div>	
+</div>
+<h1 class="text">Общественная организация ВОЛОНТЕР</h1>
+<div style="font-size:22px;" class="text"><small>Полезная деятельность во благо общества!</small></div>
+<br>
+<div class="wrapper-btn">
+	<div class="row">	
+		<div  id="btn_volonter" class="text"><small>Стать волонтёром</small></div>
+		<div id="btn_volonter" class="text"><string>Подать заявку на 
+		волонтерскую помощь<small></div>
+	</div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<div class="text" id="href"><small>Номер:<a href="#">8 (912) 8616121</a></small></div>
+<div class="text" id="href"><small>E-mail:<a href="#">volonter.online@mail.ru</a></small></div>
+<br>
+<br>
+<br>
+<br>
+<h6 class="text">СЕРВИС ВЗАИМОСВЯЗИ ВОЛОНТЕРОВ И НУЖДАЮЩИХСЯ В ПОМОЩИ</h6>
+<br>
+<br>
+<br>
+<br>
+<div class="imageSize">
+	<div class="row">
+		<div  id="image"></div>
+		<div  id="image2"></div>
+	</div>
+</div>
+<div class="imageSize">
+	<div class="row">
+<div class="text" id="img_bootom_text"><string><small>СТАНЬТЕ ВОЛОНТЕРОМ
+Чтобы стать волонтером, заполните 
+анкету, и после регистрации у 
+Вас появится возможность
+ приступать к волонтерская деятельности!</small></string></div>
+ <div class="text" id="img_bootom_text"><string><small>ПОДАЙТЕ ЗАЯВКУ НА ПОМОЩЬ
+Если Вы нуждаетесь в помощи, 
+оставьте заявку 
+на этом сайте. Кто-нибудь обязательно откликнется и 
+поможет Вам!</small></string></div>
+</div>
+</div>
+<script src="glavnaya.js"></script>
+<script src="javascript/glavnaya_bottom.js"></script>
+<script src="https://code.jquery.com/jquery-3.1.1.js"></script>
+</body>
+</html>
