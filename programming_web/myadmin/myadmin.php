955493d529a1dd5281c6b0e7e43fdf04edfc0caf
Adminfol <46747547+Adminfol@users.noreply.github.com>
2019-02-06 15:34:18 +0300

    programmer_web_onload

--- programming_web/myadmin/myadmin.php
+++ programming_web/myadmin/myadmin.php
@@ -1,3 +1,3 @@
 <?php
-echo "<script>alert('Мой админ скрипт подключен');</script>";
+echo "<script>alert('Мой админ скрипт подключен myadmin.php');</script>";
 ?>
---
+++ programming_web/script/script.js
@@ -0,0 +1,3 @@
+window.onload=function(){
+alert("Скрипт подключен script.js");
+}
