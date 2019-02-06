c715f340bae0fd04f1bc1988d7f3546117297169
Adminfol <46747547+Adminfol@users.noreply.github.com>
2019-02-06 15:22:47 +0300

    21 эвм

---
+++ programming_web/files_style.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" type="text/css" href="framework/bootstrap/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="framework/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="framework/bootstrap/css/bootstrap-grid.css">
+<link rel="stylesheet" type="text/css" href="framework/bootstrap/css/bootstrap-grid.min.css">
+<link rel="stylesheet" type="text/css" href="framework/bootstrap/css/bootstrap-reboot.css">
+<link rel="stylesheet" type="text/css" href="framework/bootstrap/css/bootstrap-reboot.min.css">
---
+++ programming_web/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Web syte</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<?php include("files_style.php"); ?>
+</head>
+<body>
+
+<?php include("myadmin/myadmin.php"); ?>
+<?php include("files_script.php"); ?>
+<script type="text/javascript" src="script/script.js"></script>
+</body>
+</html>
