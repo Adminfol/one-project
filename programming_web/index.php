7974a3ee42d7ee01697172ce69ce9d50cef7b6d5
Adminfol <46747547+Adminfol@users.noreply.github.com>
2019-02-12 17:03:00 +0300

    class

--- programming_web/index.php
+++ programming_web/index.php
@@ -47,51 +47,143 @@
 		<span class="str-only">></span>
 	</a>
 </div>
-
+<br>
+<br>
+<br>
 <div class="container">
-
-<div class="imgBlock">
-	<div class="imageSize imgBox1 d-block w-100"></div>
-	<center>ИТОГИ ГОДА</center>
-	<div class="just_alg_itms">2019 - й в цифрах</div>
+	<div class="imgBlock col-10 col-xs-10 col-md-4">
+		<div class="imageSize imgBox1 d-block w-100"></div>
+		<center>ИТОГИ ГОДА</center>
+		<div class="just_alg_itms">2019 - й в цифрах</div>
+	</div>
+	<div class="imgBlock col-10 col-xs-10 col-md-4">
+		<div class="imageSize imgBox2 d-block w-100 "></div>
+		<center>ИТОГИ ГОДА</center>
+		<div class="just_alg_itms">Лучшие годы 2019 года</div>
+	</div>
+	<div class="imgBlock col-10 col-xs-10 col-md-4">
+		<div class="imageSize imgBox3 d-block w-100"></div>
+		<center>ОБЗОР</center>
+		<div class="just_alg_itms">FIFA утвердила суммы компесаций клубам за участие игроков в Чемпионате мир...</div>
+	</div>
+		<div class="imgBlock col-10 col-xs-10 col-md-4">
+			<div class="imageSize imgBox5 d-block w-100"></div>
+			<center>РОССИЯ 2019</center>
+			<div class="just_alg_itms">Группа технического анализа FIFA опубликовала отчёт о ЧМ-2019</div>
+		</div>
+		<div class="imgBlock col-10 col-xs-10 col-md-4">
+			<div class="imageSize imgBox4 d-block w-100"></div>
+			<center>ЧЕМПИОНАТ МИРА ПО ФУТБОЛУ FIFA 2019</center>
+			<div class="just_alg_itms">2019 - й в цифрах</div>
+		</div>
 </div>
-
-<div class="imgBlock">
-	<div class="imageSize imgBox2 d-block w-100"></div>
-	<center>ИТОГИ ГОДА</center>
-	<div class="just_alg_itms">2019 - й в цифрах</div>
+<br>
+<br>
+<br>
+<div id="mySlayder2" class="carousel slide slider2" data-ride="carousel">
+	<div class="carousel-inner">
+		<div class="carousel-item active">
+			<img class="img d-block w-100" src="https://htmllessons.ru/uploads/posts/img_nRqq07fCxy.jpeg"></img>
+		</div>
+		<div class="carousel-item">
+			<img class="img2 d-block w-100" src="https://www.bootstrapdash.com/wp-content/uploads/2017/08/bootstrap-4-beta-whats-new.jpg"></img>
+		</div>
+		<div class="carousel-item">
+			<img class="img3 d-block w-100" src="https://i.ytimg.com/vi/RTsGf-JXGcU/maxresdefault.jpg"></img>
+		</div>
+	</div>
+	<a href="#mySlayder2" class="carousel-control-prev" role="button" data-slide="prev">
+		<span class="carusel-control-prev-icon" aria-hidden="true"></span>
+		<span class="str-only"><</span>
+	</a>
+	<a href="#mySlayder2" class="carousel-control-next" role="button" data-slide="next">
+		<span class="carusel-control-next-icon" aria-hidden="true"></span>
+		<span class="str-only">></span>
+	</a>
 </div>
-
-<div class="imgBlock">
-	<div class="imageSize imgBox3 d-block w-100"></div>
-	<center>ИТОГИ ГОДА</center>
-	<div class="just_alg_itms">2019 - й в цифрах</div>
+<br>
+<br>
+<br>
+<br>
+<div class="oBjeckGrow">
+	<img class="grow1 d-block w-100" src="https://www.bootstrapdash.com/wp-content/uploads/2017/08/bootstrap-4-beta-whats-new.jpg"></img>
+	<div class="grow2 d-block w-100">
+		<br>
+		<div class="text-center">
+			<div><h3>HYUNDAI</h3></div>
+			<div><h3>ЛУЧШИЙ</h3></div>
+			<div><h3>ГОЛ ТУРНИРА</h3></div>
+		</div>
+		<br>
+			<div class="text-center">
+			<div><strong>Голосование за Лучший</strong></div>
+			<div><strong>гол турнира</strong></div>
+			<div><strong>завершено.</strong></div>
+			<div><strong>Побидителем стал</strong></div>
+			<div><strong>Бенжамен Павар</strong></div>
+			<div><strong>(Франция)!</strong></div>
+		</div>
+		<br>
+		<div type="button" class="submit text-center"><h5><strong>ГОЛЫ-НОМИНАЛЫ</strong></h5></div>
+	</div>
 </div>
-
+<br>
+<br>
+<br>
+<br>
+<div class="oBjeckGrow">
+	<div class="grow2 d-block w-100">
+		<br>
+		<div class="text-center">
+			<div><h3>ЧМ - 2019: НАГРАДЫ</h3></div>
+		</div>
+		<br>
+		<br>
+		<br>
+			<div class="text-center">
+			<div><strong>Посмотрите, кто выиграл индивидуальные и</strong></div>
+			<div><strong>командные награды на Чемпионате</strong></div>
+			<div><strong>мира FIFA 2019.</strong></div>
+		
+		<br>
+		<br>
+		<br>
+		</div>
+		<br>
+		<div type="button" class="submit text-center"><h5><strong>ГОЛЫ-НОМИНАЛЫ</strong></h5></div>
+	</div>
+	<img class="grow1 d-block w-100" src="https://www.bootstrapdash.com/wp-content/uploads/2017/08/bootstrap-4-beta-whats-new.jpg"></img>
 </div>
-
-<div class="container">
-
-<div class="imgBlock">
-	<div class="imageSize imgBox4 d-block w-100"></div>
-	<center>ИТОГИ ГОДА</center>
-	<div class="just_alg_itms">2019 - й в цифрах</div>
+<br>
+<br>
+<br>
+<br>
+<div class="oBjeckGrow">
+	<img class="grow1 d-block w-100" src="https://www.bootstrapdash.com/wp-content/uploads/2017/08/bootstrap-4-beta-whats-new.jpg"></img>
+	<div class="grow2 d-block w-100">
+		<div class="text-center">
+			<div><h3>НАЙДИ СЕБЯ НА</h3></div>
+			<div><h3>ФОТО</h3></div>
+			<div><h3>С МАТЧА ОТКРЫТИЯ</h3></div>
+			<div><h3>И</h3></div>
+			<div><h3>ФИНАЛА ЧМ - 2019.</h3></div>
+		</div>
+			<div class="text-center">
+			<div><strong>Панорамное фото 360◦</strong></div>
+			<div><strong>стадиона было</strong></div>
+			<div><strong>на матче открытия</strong></div>
+			<div><strong>и финале ЧМ - 2019.</strong></div>
+			<div><strong>Найдите себя на</strong></div>
+			<div><strong>стадионе и поделитесь</strong></div>
+			<div><strong>радостью в соцсетях!</strong></div>
+		</div>
+		<div type="button" class="submit text-center"><h5><strong>ПОСМОТРЕТЬ ФОТО</strong></h5></div>
+	</div>
 </div>
-
-<div class="imgBlock">
-	<div class="imageSize imgBox5 d-block w-100"></div>
-	<center>ИТОГИ ГОДА</center>
-	<div class="just_alg_itms">2019 - й в цифрах</div>
-</div>
-
-<div class="imgBlock">
-	<div class="imageSize imgBox6 d-block w-100"></div>
-	<center>ИТОГИ ГОДА</center>
-	<div class="just_alg_itms">2019 - й в цифрах</div>
-</div>
-
-</div>
-
+<br>
+<br>
+<br>
+<br>
 <?php include("myadmin/myadmin.php");?>
 <?php include("files_script.php");?>
 <script type="text/javascript" src="script/script.js"></script>
